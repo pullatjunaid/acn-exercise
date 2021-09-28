c4c0915f6412d59905f706b6b2cd5eb5e1d37282
your_github_username <pullatjunaid@gmail.com>
2021-09-28 11:26:06 +0530

    changes in login module

--- DNS implementation/server.py
+++ DNS implementation/server.py
@@ -2,37 +2,35 @@ import socket
 import mysql.connector
 
 mydb = ""
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 print("Starting Server...")
-
-try:
-    mydb = mysql.connector.connect(host="localhost",
-                                   user="newuser",
-                                   password="password",
-                                   database="dns")
-    print("Database connected successfully...\n")
-
-except mysql.connector.Error as err:
-    print("Database connection error", err)
-    exit()
-
 s.bind(("127.0.0.1", 1234))
 
-
 def getFromDb(data, address):
+    # Connect to database 
+    try:
+        mydb = mysql.connector.connect(host="localhost",
+                                    user="newuser",
+                                    password="password",
+                                    database="dns")
+        print("Database connected successfully...\n")
+
+    except mysql.connector.Error as err:
+        print("Database connection error", err)
+        exit()
+
     dbcursor = mydb.cursor()
     dbcursor.execute("select * from dnslist where domain='" + data + "'")
     result = dbcursor.fetchall()
+    print(result)
     if len(result) > 0:
         for row in result:
             ip = row[2]
             send = s.sendto(ip.encode(), address)
     else:
-        # print('Ip address not found...')
         send = s.sendto(("IP address not found").encode(), address)
+    print(data,'\n')
 
 while True:
     data, address = s.recvfrom(1024)
